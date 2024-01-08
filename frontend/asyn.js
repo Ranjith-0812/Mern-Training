c9b1e10df598d731e576eba6a995d5fdf1dd8467
Ranjith-0812 <ranjith.r2020aids@sece.ac.in>
2024-01-08 16:01:35 +0530

    Day-5 Mern Training

---
+++ frontend/asyn.js
@@ -0,0 +1,12 @@
+let reachone = new Promise((resolve,reject)=>{
+    const reached = true;
+    if(reached) setTimeout(resolve,3000,"One REached");
+    else reject("One rejected");
+})
+
+async function(){
+    console.log("hello");
+    let res=await(reachone);
+    console.log("bye");
+    
+}
